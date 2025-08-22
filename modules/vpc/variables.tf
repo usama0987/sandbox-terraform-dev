16c2507bb82c9c3a35bb0267d48237533fbd3332
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 02:03:11 +0500

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+
+variable "name_prefix" {
+  description = "Name prefix for all resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = false
+}
