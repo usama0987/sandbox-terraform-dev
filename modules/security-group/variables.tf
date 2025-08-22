54e12805e5fe5e2ea6de07807fc8b8ac6a01a7a0
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:36:25 +0500

    Create variables.tf

---
+++ modules/security-group/variables.tf
@@ -0,0 +1,14 @@
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the security group will be created"
+  type        = string
+}
