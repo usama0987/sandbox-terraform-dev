7f7abdec6bd76206fadf1be6fd420b60531624dc
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:36:04 +0500

    Create outputs.tf

---
+++ modules/security-group/outputs.tf
@@ -0,0 +1,14 @@
+output "security_group_id" {
+  description = "ID of the shared security group"
+  value       = aws_security_group.shared.id
+}
+
+output "security_group_arn" {
+  description = "ARN of the shared security group"
+  value       = aws_security_group.shared.arn
+}
+
+output "security_group_name" {
+  description = "Name of the shared security group"
+  value       = aws_security_group.shared.name
+}
