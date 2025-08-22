84b30d685f43c06cda60744d783ce4c86e63e01e
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:34:42 +0500

    Update outputs.tf

--- environments/dev-sandbox/outputs.tf
+++ environments/dev-sandbox/outputs.tf
@@ -20,15 +20,10 @@ output "private_subnet_ids" {
   value       = module.vpc.private_subnet_ids
 }
 
-# S3 Backend Outputs
+# S3 Outputs
 output "s3_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
+  description = "Name of the S3 bucket"
   value       = module.s3_backend.s3_bucket_id
-}
-
-output "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for state locking"
-  value       = module.s3_backend.dynamodb_table_id
 }
 
 # Security Group Outputs
