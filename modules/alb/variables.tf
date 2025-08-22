38359930085a6bccb404cc022c631d2443e63d00
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:23:48 +0500

    Create variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,49 @@
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the ALB will be created"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID for the ALB"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "ARN of the SSL certificate for HTTPS listener (optional)"
+  type        = string
+  default     = null
+}
+
+variable "ssl_policy" {
+  description = "SSL policy for HTTPS listener"
+  type        = string
+  default     = "ELBSecurityPolicy-TLS-1-2-2017-01"
+}
+
+variable "health_check_path" {
+  description = "Health check path for the target group"
+  type        = string
+  default     = "/"
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection for the ALB"
+  type        = bool
+  default     = false
+}
