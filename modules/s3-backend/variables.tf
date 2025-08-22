4958e78838cb8805c88b7d7c8cfcc63edebe91aa
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:33:50 +0500

    Create variables.tf

---
+++ modules/s3-backend/variables.tf
@@ -0,0 +1,20 @@
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "force_destroy" {
+  description = "Allow deletion of non-empty bucket"
+  type        = bool
+  default     = false
+}
