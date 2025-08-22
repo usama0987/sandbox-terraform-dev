761f858f71196414c353e57439ca13f723bf5f0b
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 00:19:33 +0500

    Create outputs.tf

---
+++ environments/dev-sandbox/outputs.tf
@@ -0,0 +1,118 @@
+
+# VPC Outputs
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+# S3 Backend Outputs
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  value       = module.s3_backend.s3_bucket_id
+}
+
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  value       = module.s3_backend.dynamodb_table_id
+}
+
+# Security Group Outputs
+output "security_group_id" {
+  description = "ID of the shared security group"
+  value       = module.security_group.security_group_id
+}
+
+# ECR Outputs
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = module.ecr.repository_url
+}
+
+output "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  value       = module.ecr.repository_name
+}
+
+# ALB Outputs
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.alb_dns_name
+}
+
+output "alb_zone_id" {
+  description = "Zone ID of the Application Load Balancer"
+  value       = module.alb.alb_zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = module.alb.target_group_arn
+}
+
+# ECS Outputs
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = module.ecs.cluster_name
+}
+
+output "ecs_cluster_arn" {
+  description = "ARN of the ECS cluster"
+  value       = module.ecs.cluster_arn
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = module.ecs.service_name
+}
+
+output "ecs_service_arn" {
+  description = "ARN of the ECS service"
+  value       = module.ecs.service_arn
+}
+
+output "task_definition_arn" {
+  description = "ARN of the task definition"
+  value       = module.ecs.task_definition_arn
+}
+
+output "log_group_name" {
+  description = "Name of the CloudWatch log group"
+  value       = module.ecs.log_group_name
+}
+
+# Application URLs
+output "application_url" {
+  description = "URL to access the application"
+  value       = "http://${module.alb.alb_dns_name}"
+}
+
+# Deployment Information
+output "deployment_info" {
+  description = "Key deployment information"
+  value = {
+    environment     = local.environment
+    region          = local.region
+    vpc_cidr        = local.vpc_cidr
+    cluster_name    = module.ecs.cluster_name
+    repository_url  = module.ecr.repository_url
+    load_balancer   = module.alb.alb_dns_name
+    desired_count   = local.desired_count
+    task_cpu        = local.task_cpu
+    task_memory     = local.task_memory
+  }
+}
