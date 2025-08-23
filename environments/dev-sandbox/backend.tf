135678c01500efecd1b18e90dbbec0e635390b35
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 14:57:10 +0500

    Update backend.tf

--- environments/dev-sandbox/backend.tf
+++ environments/dev-sandbox/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tesing-terraform-tf-statefiles"        # The S3 bucket name
-    key    = "dev-sandbox/terraform.tfstate"         # The path where the state file will be stored
-    region = "us-east-1"                             # The AWS region
+    bucket = "tesing-terraform-tf-statefiles" # The S3 bucket name
+    key    = "dev-sandbox/terraform.tfstate"  # The path where the state file will be stored
+    region = "us-east-1"                      # The AWS region
   }
 }
