0e5b48066d9123a17b6e1a54b42bcd12d7b2aa50
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 10:44:14 +0500

    Update outputs.tf

--- environments/dev-sandbox/outputs.tf
+++ environments/dev-sandbox/outputs.tf
@@ -1,138 +1,49 @@
+output "task_execution_role_arn" {
+     description = "ARN of the task execution role"
+     value       = aws_iam_role.task_execution_role[0].arn
+   }
 
-# VPC Outputs
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
+   output "task_role_arn" {
+     description = "ARN of the task role"
+     value       = aws_iam_role.task_role[0].arn
+   }
 
-output "vpc_cidr_block" {
-  description = "CIDR block of the VPC"
-  value       = module.vpc.vpc_cidr_block
-}
+   output "cluster_id" {
+     description = "ID of the ECS cluster"
+     value       = aws_ecs_cluster.main.id
+   }
 
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = module.vpc.public_subnet_ids
-}
+   output "cluster_arn" {
+     description = "ARN of the ECS cluster"
+     value       = aws_ecs_cluster.main.arn
+   }
 
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = module.vpc.private_subnet_ids
-}
+   output "cluster_name" {
+     description = "Name of the ECS cluster"
+     value       = aws_ecs_cluster.main.name
+   }
 
-output "availability_zones" {
-  description = "List of availability zones used"
-  value       = module.vpc.availability_zones
-}
+   output "service_id" {
+     description = "ID of the ECS service"
+     value       = aws_ecs_service.app.id
+   }
 
-# Security Group Outputs
-output "alb_ecs_security_group_id" {
-  description = "ID of the ALB and ECS security group"
-  value       = module.security_groups.alb_ecs_security_group_id
-}
+   output "service_name" {
+     description = "Name of the ECS service"
+     value       = aws_ecs_service.app.name
+   }
 
-# ECR Outputs
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = module.ecr.repository_url
-}
+   output "task_definition_arn" {
+     description = "ARN of the task definition"
+     value       = aws_ecs_task_definition.app.arn
+   }
 
-output "ecr_repository_arn" {
-  description = "ARN of the ECR repository"
-  value       = module.ecr.repository_arn
-}
+   output "service_discovery_namespace_id" {
+     description = "ID of the service discovery namespace"
+     value       = aws_service_discovery_private_dns_namespace.main.id
+   }
 
-output "ecs_task_execution_role_arn" {
-  description = "ARN of the ECS task execution role"
-  value       = module.ecr.ecs_task_execution_role_arn
-}
-
-output "ecs_task_role_arn" {
-  description = "ARN of the ECS task role"
-  value       = module.ecr.ecs_task_role_arn
-}
-
-# ALB Outputs
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = module.alb.alb_dns_name
-}
-
-output "alb_arn" {
-  description = "ARN of the Application Load Balancer"
-  value       = module.alb.alb_arn
-}
-
-output "alb_zone_id" {
-  description = "Canonical hosted zone ID of the Application Load Balancer"
-  value       = module.alb.alb_zone_id
-}
-
-output "target_group_arn" {
-  description = "ARN of the target group"
-  value       = module.alb.target_group_arn
-}
-
-# ECS Outputs
-output "ecs_cluster_id" {
-  description = "ID of the ECS cluster"
-  value       = module.ecs.cluster_id
-}
-
-output "ecs_cluster_arn" {
-  description = "ARN of the ECS cluster"
-  value       = module.ecs.cluster_arn
-}
-
-output "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  value       = module.ecs.cluster_name
-}
-
-output "ecs_service_id" {
-  description = "ID of the ECS service"
-  value       = module.ecs.service_id
-}
-
-output "ecs_service_name" {
-  description = "Name of the ECS service"
-  value       = module.ecs.service_name
-}
-
-output "task_definition_arn" {
-  description = "ARN of the task definition"
-  value       = module.ecs.task_definition_arn
-}
-
-output "service_discovery_namespace_id" {
-  description = "ID of the service discovery namespace"
-  value       = module.ecs.service_discovery_namespace_id
-}
-
-output "cloudwatch_log_group_name" {
-  description = "Name of the CloudWatch log group"
-  value       = module.ecs.cloudwatch_log_group_name
-}
-
-# Application Access URLs
-output "application_urls" {
-  description = "URLs to access the application"
-  value = {
-    http_url    = "http://${module.alb.alb_dns_name}"
-    direct_url  = "http://${module.alb.alb_dns_name}:5000"
-  }
-}
-
-# Environment Information
-output "environment_info" {
-  description = "Environment configuration information"
-  value = {
-    environment     = "dev-sandbox"
-    region         = "us-east-1"
-    vpc_cidr       = local.vpc_cidr
-    container_port = local.container_port
-    desired_count  = local.desired_count
-    task_cpu       = local.task_cpu
-    task_memory    = local.task_memory
-  }
-}
+   output "cloudwatch_log_group_name" {
+     description = "Name of the CloudWatch log group"
+     value       = aws_cloudwatch_log_group.ecs_logs.name
+   }
