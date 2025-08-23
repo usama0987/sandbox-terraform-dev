96fb633fc49005f1a35226edf6728bbd68dd5948
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 11:20:02 +0500

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -223,39 +223,3 @@ resource "aws_appautoscaling_target" "ecs_target" {
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
-
-# Auto Scaling Policy - CPU (commented out as scaling is fixed to 1)
-# resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
-#   name               = "${var.name_prefix}-cpu-scaling"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-#
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
-#     }
-#     target_value       = var.cpu_target_value
-#     scale_in_cooldown  = 300
-#     scale_out_cooldown = 300
-#   }
-# }
-
-# Auto Scaling Policy - Memory (commented out as scaling is fixed to 1)
-# resource "aws_appautoscaling_policy" "ecs_policy_memory" {
-#   name               = "${var.name_prefix}-memory-scaling"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-#
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-#     }
-#     target_value       = var.memory_target_value
-#     scale_in_cooldown  = 300
-#     scale_out_cooldown = 300
-#   }
-# }
