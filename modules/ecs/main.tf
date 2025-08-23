60e4655eeeb456bf681eed69d51bf822a9702977
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-23 10:52:59 +0500

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -126,8 +126,8 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.task_cpu
   memory                   = var.task_memory
-  execution_role_arn       = var.task_execution_role_arn != null ? var.task_execution_role_arn : aws_iam_role.task_execution_role[0].arn
-  task_role_arn            = var.task_role_arn != null ? var.task_role_arn : aws_iam_role.task_role[0].arn
+  execution_role_arn       = coalesce(var.task_execution_role_arn, aws_iam_role.task_execution_role[0].arn)
+  task_role_arn            = coalesce(var.task_role_arn, aws_iam_role.task_role[0].arn)
 
   container_definitions = jsonencode([
     {
@@ -217,45 +217,45 @@ resource "aws_ecs_service" "app" {
 
 # Auto Scaling Target
 resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = var.max_capacity
-  min_capacity       = var.min_capacity
+  max_capacity       = 1 # Fixed to 1 container
+  min_capacity       = 1 # Fixed to 1 container
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
-# Auto Scaling Policy - CPU
-resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
-  name               = "${var.name_prefix}-cpu-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+# Auto Scaling Policy - CPU (commented out as scaling is fixed to 1)
+# resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+#   name               = "${var.name_prefix}-cpu-scaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+#
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#     target_value       = var.cpu_target_value
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 300
+#   }
+# }
 
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-    target_value       = var.cpu_target_value
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
-  }
-}
-
-# Auto Scaling Policy - Memory
-resource "aws_appautoscaling_policy" "ecs_policy_memory" {
-  name               = "${var.name_prefix}-memory-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = var.memory_target_value
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
-  }
-}
+# Auto Scaling Policy - Memory (commented out as scaling is fixed to 1)
+# resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+#   name               = "${var.name_prefix}-memory-scaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+#
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#     target_value       = var.memory_target_value
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 300
+#   }
+# }
