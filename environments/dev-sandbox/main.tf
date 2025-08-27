3e1c2e8fc6e10f065e7365b87e1788b4ae539d36
usama malik <121391282+usama0987@users.noreply.github.com>
2025-08-28 00:17:09 +0500

    Update main.tf

--- environments/dev-sandbox/main.tf
+++ environments/dev-sandbox/main.tf
@@ -155,4 +155,8 @@ module "ecs" {
   enable_deployment_circuit_breaker  = true
   enable_deployment_rollback         = true
   enable_execute_command             = false
+  
+  # Use IAM roles from ECR module to avoid conflicts
+  task_execution_role_arn             = module.ecr.ecs_task_execution_role_arn
+  task_role_arn                      = module.ecr.ecs_task_role_arn
 }
